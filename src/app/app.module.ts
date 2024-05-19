3cbb9b34c55737d6849000a4054a66ae4a8b70fc
Medhd Mehdii <mehd.mehdi00@gmail.com>
2024-05-19 15:46:29 +0200

    Project creation and deploiement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -13,7 +18,12 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideFirebaseApp(() => initializeApp({"projectId":"adoptpetonline","appId":"1:807133708196:web:9ac7aa2d3957ca3aa04b31","databaseURL":"https://adoptpetonline-default-rtdb.europe-west1.firebasedatabase.app","storageBucket":"adoptpetonline.appspot.com","apiKey":"AIzaSyDbk8jcn8aUAzGZnSGkj-bwsPCptK1JhOo","authDomain":"adoptpetonline.firebaseapp.com","messagingSenderId":"807133708196"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideStorage(() => getStorage())
   ],
   bootstrap: [AppComponent]
 })
